60c3ba7b31645ae8dfa274ecf840ba92077e86b2
Paul <101798876+Paulhakeem@users.noreply.github.com>
2023-02-22 15:12:58 +0000

    MyServices.vue

--- src/components/MyServices.vue
+++ src/components/MyServices.vue
@@ -9,30 +9,30 @@
     <div class="p-2 space-y-3 ">
     <p class="p-2">These are the skills I have but I look forward to adding more skills, because technology changes every day.</p>
     </div>
-    <div class="flex flex-columns gap-4">
-      <div>
-    <img class="w-20" src="../assets/adobep.svg"/>
-    </div>
-      <div>
-    <img class="w-20" src="../assets/adobei.svg"/>
-    </div>
+    <div class="flex flex-wrap gap-4">
     <div>
     <img class="w-20" src="../assets/html.svg"/>
+      <p class="text-gray-500 pl-6">HTML</p>
     </div>
     <div>
     <img class="w-20" src="../assets/css.svg"/>
+      <p class="text-gray-500 pl-6">CSS</p>
     </div>
       <div>
     <img class="w-20" src="../assets/tailwindcss.svg"/>
+        <p class="text-gray-500">Tailwindcss</p>
     </div>
       <div>
     <img class="w-20" src="../assets/js.svg"/>
+        <p class="text-gray-500 pl-2">JavaScript</p>
     </div>
       <div>
     <img class="w-20" src="../assets/vue.svg"/>
+        <p class="text-gray-500 pl-4">Vue.js</p>
     </div>
       <div>
     <img class="w-20" src="../assets/firebase.svg"/>
+        <p class="text-gray-500 pl-4">Firebase</p>
     </div>
     </div>
   </div>
--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -3,12 +3,12 @@
    <header class="flex itmes-center justify-between p-3 shadow-lg">
       <img class="w-20" src="../assets/LOGO.png"/>
       <div class="space-x-2 m-2">
-        <RouterLink to="/login">
+        <a href="https://github.com/Paulhakeem" target="_blank">
       <button class="bg-[#1385D2] p-2 rounded-md text-white my-4">GITHUB</button>
-        </RouterLink>
-        <RouterLink to="/signup">
+        </a>
+      
       <button class="bg-[#1385D2] text-white p-2 rounded-md">CONTACT ME</button>
-        </RouterLink>
+        
       </div>
   </header>
 </template>
